1beb3f9dff17d0201b9d82aeec4ffc4a4e01cd51
Lelihelija <lelihelija@gmail.com>
2020-10-08 12:34:31 +0300

    Updates

---
+++ precache-manifest.cb00417027a3387199f285f23b8a7280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ddc44f0f70e6578230443a0c2bc0fd",
+    "url": "/hw7/index.html"
+  },
+  {
+    "revision": "3e7593ee76be3c474fd2",
+    "url": "/hw7/static/css/main.c9167cce.chunk.css"
+  },
+  {
+    "revision": "efaac0445dc9e3285899",
+    "url": "/hw7/static/js/2.0b06fa5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw7/static/js/2.0b06fa5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7593ee76be3c474fd2",
+    "url": "/hw7/static/js/main.6f3bb142.chunk.js"
+  },
+  {
+    "revision": "acc7a1646edcb4bc190a",
+    "url": "/hw7/static/js/runtime-main.76821615.js"
+  }
+]);
